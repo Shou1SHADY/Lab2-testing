5a4aca8ac61834d1bf7782ee476a2d498f533923
Shou1SHADY <87423930+Shou1SHADY@users.noreply.github.com>
2022-05-08 23:15:07 +0200

    Add files via upload

---
+++ lab2/src/evenodd.java
@@ -0,0 +1,13 @@
+public class evenodd {
+
+
+        public String evenodd ( int n){
+
+            if ( n%2 == 0)
+                return "number is even";
+            else
+                return "number is odd";
+        }
+
+    }
+
